463045745c4f6da7f013b6a317ac6e6ae1faaa7c
Chris Jones <ipv6freely@gmail.com>
2022-05-06 15:30:50 -0700

    updated htmlcss

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -74,7 +74,7 @@ const updateConnectStatus = async () => {
     window.contract = new web3.eth.Contract(abi, contractAddress);
     loadInfo();
   } else {
-    onboardButton.innerText = "<div id=&quot;metamaskLogo&quot;></div> Connect to MetaMask!";
+    onboardButton.innerHTML = "<div id=&quot;metamaskLogo&quot;></div> Connect to MetaMask!";
     // HIDE SPINNER
     spinner.classList.add('hidden');
     notConnected.classList.remove('hidden');
