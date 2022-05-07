09a2bcf4c08047e7c0ac45ded918ceae2ae174aa
Chris Jones <ipv6freely@gmail.com>
2022-05-07 08:43:28 -0700

    updated htmlcss

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Space Bear NFT
+
+## Commands
+
+`npm run update_presale_mint_start_date`
+
+`npm run update_presale_whitelisted_addresses`
--- backend/src/config.js
+++ backend/src/config.js
@@ -46,15 +46,16 @@ const MINT_PRICE = 0.001; // USE 8 MATIC. Minting price per NFT. Rinkeby = ETH, 
 const TOKENS_PER_MINT = 10; // maximum number of NFTs a user can mint in a single transaction. CANNOT BE UPDATED!
 
 // REQUIRED CONTRACT DETAILS THAT CAN BE UPDATED LATER.
-const PUBLIC_MINT_START_DATE = "2022-05-15T14:00:00+00:00"; // MAY 1, 2022 @ 7 AM PTThis is required. Eg: 2022-02-08T11:30:48+00:00
+const PUBLIC_MINT_START_DATE = "2022-05-20T00:00:00+00:00"; // MAY 1, 2022 @ 7 AM PTThis is required. Eg: 2022-02-08T11:30:48+00:00
 
 // OPTIONAL CONTRACT DETAILS THAT CAN BE UPDATED LATER.
-const PRESALE_MINT_START_DATE = "2022-05-01T00:00:00+00:00"; // Optional. Eg: 2022-02-08T11:30:48+00:00
+const PRESALE_MINT_START_DATE = "2022-05-15T00:00:00+00:00"; // Optional. Eg: 2022-02-08T11:30:48+00:00
 const ROYALTY_SHARE = 1000; // (10%) Percentage of the token price that goes to the royalty address. 100 bps = 1%
 const ROYALTY_ADDRESS = "0x3e1be0108feeaE4B8E7BbA46F92Fe9b7FA3eba8b"; // Address that will receive the royalty
 const BASE_URI = null; // only update if you want to manually set the base uri
 const PREREVEAL_TOKEN_URI = null; // only update if you want to manually set the prereveal token uri
-const PRESALE_WHITELISTED_ADDRESSES = ["0x3e1be0108feeaE4B8E7BbA46F92Fe9b7FA3eba8b",]; // only update if you want to manually set the whitelisted addresses
+const PRESALE_WHITELISTED_ADDRESSES = []; // only update if you want to manually set the whitelisted addresses
+// const PRESALE_WHITELISTED_ADDRESSES = ["0x3e1be0108feeaE4B8E7BbA46F92Fe9b7FA3eba8b",]; // only update if you want to manually set the whitelisted addresses
 
 // ** OPTIONAL **
 let CONTRACT_ADDRESS = "YOUR CONTRACT ADDRESS"; // If you want to manually include it
