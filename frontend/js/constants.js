6cd8302475daa23beb619fd9e5c8d868f8db60af
Chris Jones <ipv6freely@gmail.com>
2022-05-07 14:46:03 -0700

    updated htmlcss

--- frontend/js/constants.js
+++ frontend/js/constants.js
@@ -12,16 +12,16 @@ const h1_public_mint = "Public Minting Open!!";
 const h2_presale_coming_soon = "";
 const h2_presale_mint = "";
 const p_presale_coming_soon = "We are working hard to launch the NFT Collection. Stay tuned for updates!";
-const p_presale_mint_not_whitelisted = "You are not whitelisted for the pre-sale.. 😢";
-const p_presale_mint_whitelisted = "You're on the whitelist for the pre-sale! 🎉";
-const p_presale_mint_already_minted = "You've already claimed your whitelist mint. Thank you! 🎉";
-const p_public_mint = "No whitelist needed. Public minting is now open! 🎉";
+const p_presale_mint_not_whitelisted = "... but you are not whitelisted for the pre-sale...";
+const p_presale_mint_whitelisted = "You're on the whitelist for the pre-sale!";
+const p_presale_mint_already_minted = "You've already claimed your whitelist mint. Thank you! ";
+const p_public_mint = "No whitelist needed. Public minting is now open!";
 const button_presale_coming_soon = "Get on the Whitelist";
 const button_presale_mint_whitelisted = "Mint Your Special NFT";
 const button_presale_mint_not_whitelisted = "Get on the Whitelist";
 const button_presale_already_minted = "Join The Community";
 const button_public_mint = "Mint Your NFT";
-const mint_failed = "Minting failed. 😢 Please try again.";
+const mint_failed = "Minting failed. Please try again.";
 
 const abi = [
   {
